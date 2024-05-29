aa6aec07130927b0706a62e0a9dc0993d7319505
Thalazor <thalazor@gmail.com>
2024-05-28 21:35:48 -0400

    Se finaliza el desafio, se integran los eventos a los botones y se calcula el total

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-precio = 400000
+precio = 400000;
 
 precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+precioSpan.innerHTML = precio;
+
+// Captura las clases y Id's Requeridos
+const cantidad = document.querySelector(".cantidad");
+const agregarCantidad = document.querySelector(".agregar");
+const quitarCantidad = document.querySelector(".quitar");
+const total = document.querySelector(".valor-total");
+
+// Se generan variables para asignar los valores
+let cantidadTotal = 0;
+cantidad.innerHTML = cantidadTotal;
+
+let totalCalculado = 0;
+total.innerHTML = totalCalculado;
+
+// Se agregan eventos para agregar o quitar productos
+agregarCantidad.addEventListener("click", () => {
+    cantidadTotal++;
+    cantidad.innerHTML = cantidadTotal;
+    totalCalculado = precio * cantidadTotal;
+    total.innerHTML = totalCalculado;
+});
+
+quitarCantidad.addEventListener("click", () => {
+    if (cantidadTotal <= 0) {
+        cantidadTotal = 0;
+    } else {
+        cantidadTotal--;
+        totalCalculado = precio * cantidadTotal;
+    }
+    cantidad.innerHTML = cantidadTotal;
+    total.innerHTML = totalCalculado;
+});
